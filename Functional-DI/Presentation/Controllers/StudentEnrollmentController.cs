78f5673913c4e2f4063ef20a5e5554d3ab346780
tjaskula <thomasv1000@hotmail.fr>
2015-01-31 10:54:22 +0100

    Add StudentEnrollmentController

---
+++ Functional-DI/Presentation/Controllers/StudentEnrollmentController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using Presentation.Models;
+
+namespace Presentation.Controllers
+{
+    public class StudentEnrollmentController : ApiController
+    {
+        public void Enroll(EnrollmentRendering r)
+        {
+            
+        }
+
+        public EnrollmentRendering Get()
+        {
+            return new EnrollmentRendering();
+        }
+    }
+}
---
+++ Functional-DI/Presentation/Models/EnrollmentRendering.cs
@@ -0,0 +1,6 @@
+﻿namespace Presentation.Models
+{
+    public class EnrollmentRendering
+    {
+    }
+}
