b202a1dc4969999b9cae4d775638124d981590e4
tjaskula <thomasv1000@hotmail.fr>
2015-02-01 22:01:33 +0100

    Add LifeTime tests for PerThread

--- Functional-DI/Presentation.Tests/LifeTimeTests.cs
+++ Functional-DI/Presentation.Tests/LifeTimeTests.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Infrastructure;
+using System.Threading;
 using Presentation.CommandHandlers;
 using Xunit;
 
@@ -8,7 +8,7 @@ namespace Presentation.Tests
     public class LifeTimeTests
     {
         [Fact]
-        public void PerThreadShouldResolveOncePerThread()
+        public void PerThreadShouldResolveTheSameForOneThread()
         {
             Func<FakeRepository> repositoryFactory = () => new FakeRepository();
 
@@ -17,6 +17,38 @@ namespace Presentation.Tests
                 var fakeRepo1 = lifeTime.PerThread(repositoryFactory);
                 var fakeRepo2 = lifeTime.PerThread(repositoryFactory);
                 Assert.Same(fakeRepo1, fakeRepo2);
+            }
+        }
+
+        [Fact]
+        public void PerThreadShouldResolveOncePerThread()
+        {
+            Func<FakeRepository> repositoryFactory = () => new FakeRepository();
+
+            using (var lifeTime = new LifeTime())
+            {
+                FakeRepository repo1 = null;
+                FakeRepository repo2 = null;
+
+                var t1 = new Thread(() =>
+                        {
+                            repo1 = lifeTime.PerThread(repositoryFactory);
+                        });
+
+                var t2 = new Thread(() =>
+                        {
+                            repo2 = lifeTime.PerThread(repositoryFactory);
+                        });
+
+                t1.Start();
+                t2.Start();
+
+                t1.Join();
+                t2.Join();
+
+                Assert.NotNull(repo1);
+                Assert.NotNull(repo2);
+                Assert.NotSame(repo1, repo2);
             }
         }
     }
