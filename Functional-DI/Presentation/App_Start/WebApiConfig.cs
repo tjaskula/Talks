65197c4941b20cea698bcf4b37abacda37719347
tjaskula <thomasv1000@hotmail.fr>
2015-02-01 13:30:59 +0100

    Add Bootstrapper

--- Functional-DI/Presentation/App_Start/WebApiConfig.cs
+++ Functional-DI/Presentation/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using Newtonsoft.Json.Serialization;
 
 namespace Presentation
 {
@@ -6,7 +8,11 @@ namespace Presentation
     {
         public static void Register(HttpConfiguration config)
         {
+            var bootstrapper = new Bootstrapper();
+
             // Web API configuration and services
+            config.Services.Replace(typeof(IHttpControllerActivator), new CustomControllerActivator(bootstrapper.RegisterDependencies()));
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ Functional-DI/Presentation/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Commands;
+using Presentation.Helpers;
+
+namespace Presentation
+{
+    public class Bootstrapper
+    {
+        public Dispatcher<ICommand> RegisterDependencies()
+        {
+            var dispatcher = new Dispatcher<ICommand>();
+
+            return dispatcher;
+        }
+    }
+}
