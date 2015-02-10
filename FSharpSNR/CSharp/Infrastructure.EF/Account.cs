48219bb34ee4b9529c81f7e54b6880c45e84518a
tjaskula <thomasv1000@hotmail.fr>
2015-02-10 23:25:14 +0100

    Add infrastructure.EF project

---
+++ FSharpSNR/CSharp/Infrastructure.EF/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.EF
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ FSharpSNR/CSharp/Infrastructure.EF/RegistrationContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Infrastructure.EF
+{
+    public class RegistrationContext : DbContext
+    {
+        public RegistrationContext() : base("RegistrationContext")
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
