04fd4ef578d9314491605da5815fb9336c4496df
tjaskula <thomasv1000@hotmail.fr>
2015-02-21 17:56:47 +0100

    Add bind function in Script

--- FSharpSNR/FSharp/Api/Script.fsx
+++ FSharpSNR/FSharp/Api/Script.fsx
@@ -2,15 +2,32 @@
 #r "FSharp.Data.TypeProviders.dll"
 #r "FSharp.Core.dll"
 #r "System.Data.Linq.dll"
-
+#r "System.ComponentModel.DataAnnotations.dll"
+ 
+#load "Common.fs" "Domain.fs" "Infrastructure.fs" "Representations.fs""AppServices.fs"
+ 
 open System
 open Microsoft.FSharp.Core
 open Microsoft.FSharp.Data.TypeProviders
-
-AppDomain.CurrentDomain.SetData("DataDirectory", "C:\Users\tjaskula\Documents\GitHub\Talks\FSharpSNR\FSharp\Api\App_Data")
-
-[<Literal>]
-let connectionString = @"Data Source=(LocalDb)\v11.0;AttachDbFileName=|DataDirectory|\FSharpSNR.mdf;Integrated Security=True;MultipleActiveResultSets=True"
-
-type dbSchema = SqlDataConnection<ConnectionString=connectionString, LocalSchemaFile="App_Data\FSharpSNR.dbml", ForceUpdate=false>
-let db = dbSchema.GetDataContext()
+open Api
+ 
+//AppDomain.CurrentDomain.SetData("DataDirectory", "C:\Users\tjaskula\Documents\GitHub\Talks\FSharpSNR\FSharp\Api\App_Data")
+//
+//[<Literal>]
+//let connectionString = @"Data Source=(LocalDb)\v11.0;AttachDbFileName=|DataDirectory|\FSharpSNR.mdf;Integrated Security=True;MultipleActiveResultSets=True"
+//
+//type dbSchema = SqlDataConnection<ConnectionString=connectionString, LocalSchemaFile="App_Data\FSharpSNR.dbml", ForceUpdate=false>
+//let db = dbSchema.GetDataContext()
+ 
+ 
+let (>>=) f1 f2 = 
+        f1 >> bind f2
+ 
+let start x =
+    Validation.validateAll
+    >> map Validation.normalizeEmail
+    >>= RegistrationService.tryConfirmEmail
+    >>= Database.findByEmailRegistration
+    >>= RegistrationService.setActivationCode
+    >>= Database.persistRegistration
+    >>= Notification.sendActivationEmail
