2f8c47890ded7c64696c50a15f9892affc5b8423
tjaskula <thomasv1000@hotmail.fr>
2015-05-05 23:39:22 +0200

    Add WordCounter application service with dummy value

---
+++ FunctionalCSharp/ObjectOriented/AppService/WordCounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOriented.AppService
+{
+    public class WordCounter
+    {
+        public int Count()
+        {
+            return 1;
+        }
+    }
+}
