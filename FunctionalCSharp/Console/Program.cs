f8f3d07805c085175ac2a28c89c64427ac53e50a
tjaskula <thomasv1000@hotmail.fr>
2015-05-05 23:21:23 +0200

    Add FileStoreReader for full book reading into the memory

--- FunctionalCSharp/Console/Program.cs
+++ FunctionalCSharp/Console/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ObjectOriented.IO;
 
 namespace Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-        }
+            var storeReader = new FileStoreReader("../../Data/monteCristo.txt");
+            var text = storeReader.Read();
+            }
     }
 }
---
+++ FunctionalCSharp/ObjectOriented/IO/FileStoreReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using ObjectOriented.AppService;
+
+namespace ObjectOriented.IO
+{
+    public class FileStoreReader : IStoreReader<string>
+    {
+        private readonly string _filePath;
+
+        public FileStoreReader(string filePath)
+        {
+            if (string.IsNullOrWhiteSpace(filePath))
+                throw new ArgumentNullException("filePath");
+
+            if (!File.Exists(filePath))
+                throw new InvalidOperationException(string.Format("The file path '{0}' doesn't exist"));
+
+            _filePath = filePath;
+        }
+
+        public string Read()
+        {
+            return File.ReadAllText(_filePath);
+        }
+    }
+}
