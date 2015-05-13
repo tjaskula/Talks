00d819d023d59e82c8174846e3be5d2471cae2be
tjaskula <thomasv1000@hotmail.fr>
2015-05-13 22:41:48 +0200

    Add WordCounter + UT

---
+++ FunctionalCSharp/ObjectOriented.Tests/Parser/WordCounterTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using ObjectOriented.Parser;
+using Xunit;
+using Xunit.Extensions;
+
+namespace ObjectOriented.Tests.Parser
+{
+    public class WordCounterTests
+    {
+        [Fact]
+        public void ShouldReturnZeroWhenNullText()
+        {
+            var wordCounter = new WordCounter();
+
+            Assert.Equal(0, wordCounter.Count(null));
+        }
+
+        [Fact]
+        public void ShouldReturnZeroWhenEmptyText()
+        {
+            var wordCounter = new WordCounter();
+
+            Assert.Equal(0, wordCounter.Count(null));
+        }
+
+        [Theory, PropertyData("GetText")]
+        public void ShouldReturnCountWhenText(string text, int expectedCount)
+        {
+            var wordCounter = new WordCounter();
+
+            Assert.Equal(expectedCount, wordCounter.Count(text));
+        }
+
+        public static IEnumerable<object[]> GetText
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] {"One", 1},
+                    new object[] {"One two", 2},
+                    new object[] {"One two three four five six seven eight nine ten", 10}
+                };
+            }
+        }
+    }
+}
---
+++ FunctionalCSharp/ObjectOriented/Parser/WordCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ObjectOriented.Parser
+{
+    public class WordCounter
+    {
+        public int Count(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return 0;
+
+            return Regex.Matches(text, @"\S+").Count;
+        }
+    }
+}
