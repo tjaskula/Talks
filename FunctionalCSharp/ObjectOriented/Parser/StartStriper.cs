4e8f3022924c6c6dc0894b122f72b3c783fde33a
tjaskula <thomasv1000@hotmail.fr>
2015-05-15 13:36:40 +0200

    Refactoring StartStriper

--- FunctionalCSharp/ObjectOriented/Parser/StartStriper.cs
+++ FunctionalCSharp/ObjectOriented/Parser/StartStriper.cs
@@ -9,9 +9,10 @@ namespace ObjectOriented.Parser
             if (string.IsNullOrWhiteSpace(input))
                 throw new ArgumentNullException("input");
 
-            int startLineIndx = input.IndexOf("*** START", StringComparison.InvariantCulture);
-            int endOfLineIndx = input.IndexOf("***", startLineIndx + 10, StringComparison.InvariantCulture);
-            return input.Remove(0, endOfLineIndx + 3);
+            const string startPattern = "*** START";
+            int startLineIndx = input.IndexOf(startPattern, StringComparison.OrdinalIgnoreCase);
+            int endOfLineIndx = input.IndexOf("***", startLineIndx + startPattern.Length, StringComparison.OrdinalIgnoreCase);
+            return input.Remove(0, endOfLineIndx + "***".Length);
         }
     }
 }
