9a71e102d84aabaeebff7e5b8d8bdc6bdf7e8528
tjaskula <thomasv1000@hotmail.fr>
2015-05-16 17:32:24 +0200

    Add ParserCombinator and bootstrap

--- FunctionalCSharp/Console/Bootstraper.cs
+++ FunctionalCSharp/Console/Bootstraper.cs
@@ -11,7 +11,9 @@ namespace Console
 
         public Resolver Initialize()
         {
-            var wordCounter = new WordCounterService(new WordCounter(), new StartStriper(), new FileStoreReader());
+            var parsers = new ParserCombinator();
+            parsers.AddNext(new StartStriper()).AddNext(new EndStriper());
+            var wordCounter = new WordCounterService(new WordCounter(), parsers, new FileStoreReader());
             _resolver.Register(wordCounter);
 
             return _resolver;
---
+++ FunctionalCSharp/ObjectOriented/Parser/ParserCombinator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectOriented.Parser
+{
+    public class ParserCombinator : IParser<string>
+    {
+        private readonly IList<IParser<string>> _parsers = new List<IParser<string>>(); 
+        
+        public string Parse(string input)
+        {
+            string result = input;
+            foreach (var parser in _parsers)
+                result = parser.Parse(result);
+            return result;
+        }
+
+        public ParserCombinator AddNext(IParser<string> parser)
+        {
+            if (parser == null)
+                throw new ArgumentNullException("parser");
+
+            _parsers.Add(parser);
+
+            return this;
+        }
+    }
+}
