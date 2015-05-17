4bac2c4a0fb6fd4394db4b860c4f6eb8b2794c2a
tjaskula <thomasv1000@hotmail.fr>
2015-05-17 23:52:19 +0200

    Functional ParseResult

--- FunctionalCSharp/Console/Program.cs
+++ FunctionalCSharp/Console/Program.cs
@@ -1,4 +1,8 @@
-﻿using ObjectOriented.ApplicationSerivice;
+﻿using Functional;
+using ObjectOriented.ApplicationSerivice;
+using ObjectOriented.IO;
+using ObjectOriented.Parser;
+using System.Linq;
 
 namespace Console
 {
@@ -10,14 +14,50 @@ namespace Console
         {
             const string path = "../../Data/monteCristo.txt";
 
-            var bootstrapper = new Bootstraper();
-            var resolver = bootstrapper.Initialize();
+            //var bootstrapper = new Bootstraper();
+            //var resolver = bootstrapper.Initialize();
 
-            var wordCounter = resolver.Resolve<WordCounterService>();
-            var number = wordCounter.CountInFile(path);
+            //var wordCounter = resolver.Resolve<WordCounterService>();
+            //var number = wordCounter.CountInFile(path);
 
-            Console.WriteLine("Number of words : {0}", number);
-            Console.ReadKey();
+            //Console.WriteLine("Number of words : {0}", number);
+            //Console.ReadKey();
+
+
+            var content = new FileStoreReader().Read(path);
+
+            Func<string, Parsers.ParseResult<string>> startSriper = input =>
+                                {
+                                    if (string.IsNullOrWhiteSpace(input))
+                                        return new Parsers.Error<string>("Cannot process an empty input");
+
+                                    const string startPattern = "*** START";
+
+                                    int startLineIndx = input.IndexOf(startPattern, StringComparison.OrdinalIgnoreCase);
+                                    int endOfLineIndx = input.IndexOf("***", startLineIndx + startPattern.Length, StringComparison.OrdinalIgnoreCase);
+                                    string remaining = input.Remove(0, endOfLineIndx + "***".Length);
+
+                                    return new Parsers.Success<string>(remaining);
+                                };
+
+            Func<string, Parsers.ParseResult<string>> endSriper = input =>
+                                {
+                                    if (string.IsNullOrWhiteSpace(input))
+                                        return new Parsers.Error<string>("Cannot process an empty input");
+
+                                    const string endPattern = "*** END";
+                                    int endLineIndx = input.IndexOf(endPattern, StringComparison.OrdinalIgnoreCase);
+                                    string remaining = input.Remove(endLineIndx);
+
+                                    return new Parsers.Success<string>(remaining);
+                                };
+
+            var pipline = new Parsers.Success<string>(content).Bind(startSriper).Bind(endSriper);
+
+            var result = from a in content.ToParseResult()
+                from b in startSriper(a)
+                from c in endSriper(b)
+                select c;
         }
     }
 }
---
+++ FunctionalCSharp/Functional/Parsers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Functional
+{
+    public static class Parsers
+    {
+        public abstract class ParseResult<T>
+        {
+        }
+
+        public class Success<T> : ParseResult<T>
+        {
+            public Success(T parsed)
+            {
+                Parsed = parsed;
+            }
+
+            public T Parsed { get; private set; }
+        }
+
+        public class Error<T> : ParseResult<T>
+        {
+            public Error(string message)
+            {
+                Message = message;
+            }
+
+            public string Message { get; private set; }
+        }
+
+        public static ParseResult<R> Bind<A, R>(this ParseResult<A> parsed, Func<A, ParseResult<R>> function)
+        {
+            var parseda = parsed as Success<A>;
+
+            return parseda == null
+                ? new Error<R>(((Error<A>)parsed).Message)
+                : function(parseda.Parsed);
+        }
+
+        public static ParseResult<T> ToParseResult<T>(this T value)
+        {
+            return new Success<T>(value);
+        }
+
+        public static ParseResult<C> SelectMany<A, B, C>(this ParseResult<A> a, Func<A, ParseResult<B>> func, Func<A, B, C> select)
+        {
+            return a.Bind(aval =>
+                    func(aval).Bind(bval =>
+                    select(aval, bval).ToParseResult()));
+        }
+
+        public delegate ParseResult<R> Parser<T, R>(ParseResult<T> input);
+
+        //public static Parser<T, V> ApplyFunction<T, R, V>(this Parser<T, R> parser, Func<T, R, Parser<R, V>> func)
+        //{
+        //    return input =>
+        //    {
+        //        var parsed = parser(input);
+        //        var result = parsed.ApplyFunction()
+        //    };
+        //}
+    }
+}
