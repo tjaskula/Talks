5e86b4daaa6c99fe5c55c82b4c6c60a2299c3f5f
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-05-25 19:23:14 +0200

    Delete IHandlerFactory because not used

--- Alt.NET-DI/Api.Altnetdi.Demo/Api.Rest.Infrastructure/IHandlerFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Api.Rest.Infrastructure
-{
-	public interface IHandlerFactory
-	{
-		IEnumerable<IMessageHandler> GetHandlersFor<TMessage>() where TMessage : Message;
-	}
-}
