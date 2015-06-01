5d77cdca3c1113c07f1dd20b3262988c12e80f4f
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-06-01 19:40:59 +0200

    Delete OrderLine because not used

--- Alt.NET-DI/Api.Altnetdi.Demo/Api.Rest.Domain/OrderLine.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Api.Rest.Domain
-{
-	public class OrderLine
-	{
-		public int Id { get; private set; }
-		public decimal ProductCost { get; private set; }
-		public string ProductName { get; private set; }
-	}
-}
