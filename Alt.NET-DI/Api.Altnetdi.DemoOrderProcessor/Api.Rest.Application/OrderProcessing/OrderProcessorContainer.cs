6e200eb8b66fc921806e6a65dee4bc96eef730e0
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-06-13 16:27:32 +0200

    Uncomment

--- Alt.NET-DI/Api.Altnetdi.DemoOrderProcessor/Api.Rest.Application/OrderProcessing/OrderProcessorContainer.cs
+++ Alt.NET-DI/Api.Altnetdi.DemoOrderProcessor/Api.Rest.Application/OrderProcessing/OrderProcessorContainer.cs
@@ -8,42 +8,42 @@ using Castle.Windsor;
 
 namespace Api.Rest.Application.OrderProcessing
 {
-	//public class OrderProcessorContainer : IOrderProcessor
-	//{
-	//    private readonly IWindsorContainer _container;
-	//    public Guid InstanceId { get; private set; }
+    public class OrderProcessorContainer : IOrderProcessor
+    {
+        private readonly IWindsorContainer _container;
+        public Guid InstanceId { get; private set; }
 
-	//    public OrderProcessorContainer(IWindsorContainer container)
-	//    {
-	//        _container = container;
-	//        InstanceId = Guid.NewGuid();
-	//    }
+        public OrderProcessorContainer(IWindsorContainer container)
+        {
+            _container = container;
+            InstanceId = Guid.NewGuid();
+        }
 
-	//    public SuccessResult Process(Order order)
-	//    {
-	//        var validator = _container.Resolve<IOrderValidator>();
-	//        var shipper = _container.Resolve<IOrderShipper>();
+        public SuccessResult Process(Order order)
+        {
+            var validator = _container.Resolve<IOrderValidator>();
+            var shipper = _container.Resolve<IOrderShipper>();
 
-	//        bool isValid = validator.Validate(order);
-	//        if (isValid)
-	//        {
-	//            Collect(order);
-	//            shipper.Ship(order);
-	//        }
+            bool isValid = validator.Validate(order);
+            if (isValid)
+            {
+                Collect(order);
+                shipper.Ship(order);
+            }
 
-	//        return CreateStatus(isValid);
-	//    }
+            return CreateStatus(isValid);
+        }
 
-	//    private void Collect(Order order)
-	//    {
-	//        //User user = _userContext.GetCurrentUser();
-	//        //Price price = order.GetPrice(_exchange, _userContext);
-	//        //_receivable.Collect(user, price);
-	//    }
+        private void Collect(Order order)
+        {
+            User user = _userContext.GetCurrentUser();
+            Price price = order.GetPrice(_exchange, _userContext);
+            _receivable.Collect(user, price);
+        }
 
-	//    private SuccessResult CreateStatus(bool isValid)
-	//    {
-	//        return isValid ? SuccessResult.Success : SuccessResult.Failed;
-	//    }
-	//}
+        private SuccessResult CreateStatus(bool isValid)
+        {
+            return isValid ? SuccessResult.Success : SuccessResult.Failed;
+        }
+    }
 }
