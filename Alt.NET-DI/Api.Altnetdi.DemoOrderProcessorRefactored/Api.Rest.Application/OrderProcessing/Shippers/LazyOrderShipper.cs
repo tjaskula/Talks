2edbd7404afb0d5854c4e6958cb0c058b2ec1fb1
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-07-06 09:56:13 +0200

    Uncomment LazyOrderShipper

--- Alt.NET-DI/Api.Altnetdi.DemoOrderProcessorRefactored/Api.Rest.Application/OrderProcessing/Shippers/LazyOrderShipper.cs
+++ Alt.NET-DI/Api.Altnetdi.DemoOrderProcessorRefactored/Api.Rest.Application/OrderProcessing/Shippers/LazyOrderShipper.cs
@@ -1,20 +1,23 @@
-﻿namespace Api.Rest.Application.OrderProcessing.Shippers
+﻿using Api.Rest.Domain;
+using Api.Rest.Domain.Processors;
+
+namespace Api.Rest.Application.OrderProcessing.Shippers
 {
-	//public class LazyOrderShipper : IOrderShipper
-	//{
-	//    private readonly IOrderShipperFactory _orderShipperFactory;
-	//    private IOrderShipper _orderShipper;
+    public class LazyOrderShipper : IOrderShipper
+    {
+        private readonly IOrderShipperFactory _orderShipperFactory;
+        private IOrderShipper _orderShipper;
 
-	//    public LazyOrderShipper(IOrderShipperFactory orderShipperFactory)
-	//    {
-	//        _orderShipperFactory = orderShipperFactory;
-	//    }
+        public LazyOrderShipper(IOrderShipperFactory orderShipperFactory)
+        {
+            _orderShipperFactory = orderShipperFactory;
+        }
 
-	//    public void Ship(Order order)
-	//    {
-	//        if (_orderShipper == null)
-	//            _orderShipper = _orderShipperFactory.GetOrderShipper();
-	//        _orderShipper.Ship(order);
-	//    }
-	//}
+        public void Ship(Order order)
+        {
+            if (_orderShipper == null)
+                _orderShipper = _orderShipperFactory.GetOrderShipper();
+            _orderShipper.Ship(order);
+        }
+    }
 }
