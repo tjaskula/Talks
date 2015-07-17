6b6b6811f19c54cd7be57f5e2112c89982d2a63f
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-07-17 13:42:56 +0200

    Add TestInitialize for common RegisterController setup

--- FSharpSNR/CSharp/Refactored/Api.Tests/RegisterControllerTests.cs
+++ FSharpSNR/CSharp/Refactored/Api.Tests/RegisterControllerTests.cs
@@ -18,19 +18,25 @@ namespace Api.Tests
     [TestClass]
     public class RegisterControllerTests
     {
-        [TestMethod]
-        public void RegisterShouldReturnBadRequestWhenNullRepresentation()
+        private RegisterController _controller;
+
+        [TestInitialize]
+        public void SetupController()
         {
             var accountRepositoryMock = new Mock<IAccountRepository>();
             var representationValidatorMock = new Mock<IRepresentationValidator>();
             var registrationServiceMock = new Mock<IRegistrationService>();
             var notifierMock = new Mock<INotifier>();
 
-            var controller = new RegisterController(accountRepositoryMock.Object, representationValidatorMock.Object, registrationServiceMock.Object, notifierMock.Object);
-            controller.Request = new HttpRequestMessage(HttpMethod.Post, new Uri("http://localhost/api/register"));
-            controller.ControllerContext = new HttpControllerContext(new HttpConfiguration(), new HttpRouteData(new HttpRoute("api/register")), controller.Request);
+            _controller = new RegisterController(accountRepositoryMock.Object, representationValidatorMock.Object, registrationServiceMock.Object, notifierMock.Object);
+            _controller.Request = new HttpRequestMessage(HttpMethod.Post, new Uri("http://localhost/api/register"));
+            _controller.ControllerContext = new HttpControllerContext(new HttpConfiguration(), new HttpRouteData(new HttpRoute("api/register")), _controller.Request);
+        }
 
-            var result = controller.Register(null) as InvalidModelStateResult;
+        [TestMethod]
+        public void RegisterShouldReturnBadRequestWhenNullRepresentation()
+        {
+            var result = _controller.Register(null) as InvalidModelStateResult;
             var response = result.ExecuteAsync(new CancellationToken()).Result;
 
             Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
