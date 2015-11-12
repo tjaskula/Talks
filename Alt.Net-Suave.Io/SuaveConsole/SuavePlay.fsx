d90fe727d391e2dedba2e39ea87ce20d40dd0e77
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-11-12 09:12:10 +0100

    Add news server in suave

--- Alt.Net-Suave.Io/SuaveConsole/SuavePlay.fsx
+++ Alt.Net-Suave.Io/SuaveConsole/SuavePlay.fsx
@@ -69,58 +69,122 @@ printfn "http://openweathermap.org/img/w/%s.png" paris.Weather.[0].Icon
 // 4. News server
 /// Helper function that returns nice HTML page with title & body
 
+open System
 open System.IO
+// ----------------------------------------------------------------------------
+// Helper functions for formatting long text & date
+// ----------------------------------------------------------------------------
 
-let simplePage title body =
-  File.ReadAllText(Path.Combine(__SOURCE_DIRECTORY__, "web/blank.html"))
-    .Replace("[TITLE]", title).Replace("[BODY]", body)
+open System.Text.RegularExpressions
 
-// The following uses F# Data to define types that you'll need:
+let stripHtml (html:string) =
+  Regex.Replace(html, "<.*?>", "")
+
+let formatText length (comment:string) =
+  let comment = comment.Replace("\n", " ").Replace("\r", " ")
+  let short = comment.Substring(0, min length (comment.Length))
+  if short.Length < comment.Length then short + "..." else short
+
+let formatDate (date:DateTime) =
+  let ts = DateTime.Now - date
+  if ts.TotalDays > 1.0 then sprintf "%d days ago" (int ts.TotalDays)
+  elif ts.TotalHours > 1.0 then sprintf "%d hours ago" (int ts.TotalHours)
+  elif ts.TotalMinutes > 1.0 then sprintf "%d minutes ago" (int ts.TotalMinutes)
+  else "just now"
+
+// ----------------------------------------------------------------------------
+// Getting blog news via RSS feed
+// ----------------------------------------------------------------------------
 
 type RssFeed = XmlProvider<"http://fpish.net/rss/blogs/tag/1/f~23">
-type GithubSearch = JsonProvider<"samples/github-search.json">
-type GithubEvents = JsonProvider<"samples/github-events.json">
 
-
-let news = async {
-  // Read the RSS feed
+let getFeedNews () = async {
   let! rss = Http.AsyncRequestString("http://fpish.net/rss/blogs/tag/1/f~23")
+  let feed = RssFeed.Parse(rss)
+  let html =
+    [ for item in feed.Channel.Items do
+        yield "<li>"
+        yield sprintf "<h3><a href=\"%s\">%s</a></h3>" item.Link item.Title
+        yield sprintf "<p class=\"date\">%s</p>" (formatDate item.PubDate)
+        yield sprintf "<p>%s</p>" (formatText 500 (stripHtml (defaultArg item.Description "")))
+        yield "</li>" ]
+  return String.concat "" html }
 
-  // Get recent starred F# projects from GitHub
+// ----------------------------------------------------------------------------
+// Searching for starred F# projects on GitHub
+// ----------------------------------------------------------------------------
+
+type GithubSearch = JsonProvider<"samples/github-search.json">
+
+let getGithubProjects () = async {
   let! res =
     Http.AsyncRequestString
       ( "https://api.github.com/search/repositories?q=language:fsharp&sort=stars&order=desc",
         httpMethod="GET", headers=[
           HttpRequestHeaders.Accept "application/vnd.github.v3+json";
           HttpRequestHeaders.UserAgent "SuaveDemo"] )
-
-  // Finally, to request the F# org events, you can use the
-  // following URL: https://api.github.com/orgs/fsharp/events
-  // (Otherwise, the request you need is exactly the same)
-  return 0 }
-
-
-let getFeedNews () = async {
-  // TODO: Format the news from RSS feed as HTML and return it
+  let search = GithubSearch.Parse(res)
   let html =
-    [ for item in 1 .. 10 do
+    [ for it in search.Items do
         yield "<li>"
-        yield sprintf "<h3><a href=\"%s\">Nothing happened (#%d)</a></h3>" "#" item
-        yield sprintf "<p class=\"date\">%s</p>" "Just now"
-        yield sprintf "<p>Nothing happened, nothing is happening and nothing will ever happen</p>"
+        yield sprintf "<h3><a href=\"%s\">%s</a></h3>" it.HtmlUrl it.Name
+        yield sprintf "<p>%s</p>" (defaultArg it.Description "")
         yield "</li>" ]
   return String.concat "" html }
+
+// ----------------------------------------------------------------------------
+// Searching for recent GitHub events in 'fsharp' organization
+// ----------------------------------------------------------------------------
+
+type GithubEvents = JsonProvider<"samples/github-events.json">
+
+let getGithubEvents () = async {
+  let! eventsJson =
+    Http.AsyncRequestString
+      ( "https://api.github.com/orgs/fsharp/events",
+        httpMethod="GET", headers=[
+          HttpRequestHeaders.Accept "application/vnd.github.v3+json";
+          HttpRequestHeaders.UserAgent "SuaveDemo"] )
+  let events = GithubEvents.Parse(eventsJson)
+  let html =
+    [ for evt in events do
+        if evt.Payload.Comment.IsSome || evt.Payload.PullRequest.IsSome then
+          yield "<li>"
+          yield sprintf "<img src=\"%s\" />" evt.Actor.AvatarUrl
+          yield sprintf "<p>%s <a href=\"http://github.com/%s\">@%s</a>"
+                  (formatDate evt.CreatedAt) evt.Actor.Login evt.Actor.Login
+
+          match evt.Payload.Comment, evt.Payload.PullRequest with
+          | Some cmt, _ ->
+              yield sprintf "<a href=\"%s\">commented</a>:</p>" cmt.HtmlUrl
+              yield sprintf "<p class=\"body\">%s</p>" (formatText 100 cmt.Body)
+          | _, Some pr ->
+              let action = evt.Payload.Action.Value
+              let action = if action = "closed" && pr.Merged then "merged" else action
+              yield sprintf "<a href=\"%s\">%s</a>:</p>" pr.HtmlUrl action
+              yield sprintf "<p class=\"body\">%s</p>" (formatText 100 pr.Title)
+          | _ -> ()
+          yield "</li>" ]
+  return String.concat "" html }
+
+// ----------------------------------------------------------------------------
+// Minimal server to host the site
+// ----------------------------------------------------------------------------
 
 let template = Path.Combine(__SOURCE_DIRECTORY__, "web/index.html")
 let html = File.ReadAllText(template)
 
 /// The main handler for Suave server!
 let app_4 ctx = async {
-  let! data = [ getFeedNews() ] |> Async.Parallel
+  let! data =
+    [ getFeedNews()
+      getGithubEvents()
+      getGithubProjects() ]
+    |> Async.Parallel
   let html =
     html.Replace("[FEED-NEWS]", data.[0])
-        .Replace("[GITHUB-NEWS]", "")
-        .Replace("[GITHUB-PROJECTS]", "")
+        .Replace("[GITHUB-NEWS]", data.[1])
+        .Replace("[GITHUB-PROJECTS]", data.[2])
   return! ctx |> Successful.OK(html) }
 
 let cts4 = startServer app_4
