5a3162866bdd4ebc11ee53cae3b927d40031190e
Tomasz Jaskula <thomasv1000@hotmail.fr>
2015-11-12 01:01:53 +0100

    Add async sample

--- Alt.Net-Suave.Io/SuaveConsole/SuavePlay.fsx
+++ Alt.Net-Suave.Io/SuaveConsole/SuavePlay.fsx
@@ -7,5 +7,31 @@ open Suave.Web
 // 1. The hello world example
 startWebServer defaultConfig (OK "Hello ALT.NET Paris World!")
 
+let startServer app =
+  let _, server = startWebServerAsync defaultConfig app
+  let cts = new System.Threading.CancellationTokenSource()
+  Async.Start(server, cts.Token)
+  cts
+
+let stopServer (cts : System.Threading.CancellationTokenSource) =
+  cts.Cancel()
+
 // 2. Defining simple application
-let app = OK("Hello Alt.Net World!")
+let app = OK("Hello ALT.Net Paris World!")
+
+let cts = startServer app
+stopServer cts
+
+open Suave.Types
+open Suave.Http
+open Suave.Http.RequestErrors
+open Suave.Http.Applicatives
+
+let app_2 : WebPart =
+  choose
+    [ path "/" >>= OK "See <a href=\"/add/40/2\">40 + 2</a>"
+      pathScan "/add/%d/%d" (fun (a,b) -> OK(string (a + b)))
+      NOT_FOUND "Found no handlers" ]
+
+let cts2 = startServer app_2
+stopServer cts2
