9f1a4afd1d55581c1aa790fa4ee297027035bd13
Thomas Jaskula <thomasv1000@hotmail.fr>
2015-12-03 10:43:27 +0100

    Basic string to string State monad implementation

--- Alt.NET-Refunctoring/ReFuncToring/Program.cs
+++ Alt.NET-Refunctoring/ReFuncToring/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ReFuncToring
+﻿namespace ReFuncToring
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string query = "Select something";
+            string view;
+
+            var readData = StateMan.GetStateFromData();
+            var runRules = StateMan.GetRules();
+            var mapViews = StateMan.GetMappers();
+
+            var useCasePipline = from data in readData(query)
+                                 from executedRules in runRules(data)
+                                 from mappedViews in mapViews(executedRules)
+                                 select mappedViews;
+
+            view = useCasePipline.IsSuccess ? useCasePipline.FromState() : "KO";
+
+            System.Console.WriteLine("The view is executed : {0}", view);
+            System.Console.ReadKey();
         }
     }
 }
---
+++ Alt.NET-Refunctoring/ReFuncToring/State.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ReFuncToring
+{
+    public static partial class StateMan
+    {
+        public static StatePiper<string, string> GetStateFromData()
+        {
+            return GetRepoStateFunc().ToStatePiper();
+        }
+
+        public static Func<string, Result<string>> GetRepoStateFunc()
+        {
+            return query =>
+            {
+                if (string.IsNullOrWhiteSpace(query))
+                    return new Error<string>("Cannot process an empty input");
+
+                // execute de query
+
+                var result = "Some result from the database";
+
+                return new Success<string>(result);
+            };
+        }
+
+        public static StatePiper<string, string> GetRules()
+        {
+            return GetRulesFunc().ToStatePiper();
+        }
+
+        public static Func<string, Result<string>> GetRulesFunc()
+        {
+            return input =>
+            {
+                if (string.IsNullOrWhiteSpace(input))
+                    return new Error<string>("Cannot process an empty input");
+
+                string blResult = "Business logic executed";
+
+                return new Success<string>(blResult);
+            };
+        }
+
+        public static StatePiper<string, string> GetMappers()
+        {
+            return GetMappersFunc().ToStatePiper();
+        }
+
+        public static Func<string, Result<string>> GetMappersFunc()
+        {
+            return input =>
+            {
+                if (string.IsNullOrWhiteSpace(input))
+                    return new Error<string>("Cannot process an empty input");
+
+                string mappingResult = "Object Mapped";
+
+                return new Success<string>(mappingResult);
+            };
+        }
+    }
+}
