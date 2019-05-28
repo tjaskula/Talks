6711356cae2f6b3d3158b63a207244a432337f22
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-05-28 23:57:47 +0200

    Making stateful projection

--- FSharpEs/AccountingEs/AccountingEs/EventHandler.fs
+++ FSharpEs/AccountingEs/AccountingEs/EventHandler.fs
@@ -4,13 +4,20 @@ open Accounting
 
 type EventHandler() =
     
+    let mutable balance = 0M
+    
     let accountId = function AccountId id -> id
     
     member this.Handle =
         function
         | Opened a ->
             printfn "Opened account '%d'" (accountId a.AccountId)
+            printfn "The current balance for account '%d' is '%M' euros" (accountId a.AccountId) balance
         | Deposited a ->
+            balance <- balance + a.Amount
             printfn "Deposited '%M' euros on account '%d'" a.Amount (accountId a.AccountId)
+            printfn "The current balance for account '%d' is '%M' euros" (accountId a.AccountId) balance
         | Withdrawn a ->
+            balance <- balance - a.Amount
             printfn "Withdrawn '%M' euros from account '%d'" a.Amount (accountId a.AccountId)
+            printfn "The current balance for account '%d' is '%M' euros" (accountId a.AccountId) balance
